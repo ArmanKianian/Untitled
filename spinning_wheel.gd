eae1110f8a18715b2973d404a7e6afcda1aaa3d9
ArmanKianian <kianianarman1@gmail.com>
2026-06-20 16:05:40 +0330

    Spinning wheel v0.5
    now spinning wheel chose a item randomly based on their weights(chance of getting each item)
    
    still has no visual graphics, just printed in consol.

---
+++ spinning_wheel.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+# wheel items with chance of getting
+var items: Array = [
+	{"name": "Item1", "chance": 10.0},
+	{"name": "Item2", "chance": 10.0},
+	{"name": "Item3", "chance": 10.0},
+	{"name": "Item4", "chance": 10.0},
+	{"name": "Item5", "chance": 10.0},
+	{"name": "Item6", "chance": 10.0},
+	{"name": "Item7", "chance": 10.0},
+	{"name": "Item8", "chance": 10.0},
+	{"name": "Item9", "chance": 10.0},
+	{"name": "Item10", "chance": 1000.0},
+] 
+
+func _ready() -> void:
+	spin()
+
+func spin():
+	var chosen = pick_weighted_random_item()
+	print(chosen)
+
+func pick_weighted_random_item():
+	var chance_sum: float = 0
+	for item in items:
+		chance_sum += item["chance"]
+	
+	var random_chance = randf_range(0, chance_sum)
+	for item in items:
+		random_chance -= item["chance"]
+		if random_chance <= 0:
+			return item
+	return items[0]

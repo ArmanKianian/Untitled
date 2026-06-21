26a3b6720e25a5d526fe38faa3ac714b2cc71dc4
ArmanKianian <kianianarman1@gmail.com>
2026-06-21 10:45:48 +0330

    refactor
    cleaned game scene, codes
    and fixed some bugs

--- drag_drop.gd
+++ drag_drop.gd
@@ -4,16 +4,17 @@ extends Sprite2D
 var drag = false
 var mouse_offset
 var future_position = global_position
-var start_position = global_position
-var area_position
+@export var current_area: Area2D
+var start_area
 var droppable = false
 
 func _ready() -> void:
+	start_area = current_area
 	for area in areas:
 		area.mouse_entered.connect(_on_mouse_entered.bind(area))
 		area.mouse_exited.connect(_on_mouse_exited)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if drag == true:
 		future_position = get_global_mouse_position() - mouse_offset
 	global_position.x = move_toward(global_position.x, future_position.x, 30)
@@ -23,18 +24,24 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("LMB") and get_rect().has_point(get_local_mouse_position()):
 		mouse_offset = get_local_mouse_position()
 		drag = true
-	elif event.is_action_released("LMB"):
+	elif event.is_action_released("LMB") and drag == true:
 		drag = false
 		if droppable == false:
-			future_position = start_position
+			future_position = start_area.position
 		else:
-			future_position = area_position
-			start_position = area_position
-
+			if current_area.item == null:
+				start_area.item = null
+				current_area.item = self
+				start_area = current_area
+				future_position = current_area.position
+			else:
+				future_position = start_area.position
+				
 func _on_mouse_entered(area):
-	area_position = area.position
-	droppable = true
+	if drag == true:
+		current_area = area
+		droppable = true
 
 func _on_mouse_exited():
-	area_position = start_position
+	current_area = start_area
 	droppable = false
--- spinning_wheel.gd
+++ spinning_wheel.gd
@@ -11,16 +11,14 @@ var items: Array = [
 	{"name": "Item7", "chance": 10.0},
 	{"name": "Item8", "chance": 10.0},
 	{"name": "Item9", "chance": 10.0},
-	{"name": "Item10", "chance": 1000.0},
+	{"name": "Item10", "chance": 10.0},
 ] 
-
-func _ready() -> void:
-	spin()
 
 func spin():
 	var chosen = pick_weighted_random_item()
 	print(chosen)
 
+# Normalize Chance then pick one random based on weight(chance)
 func pick_weighted_random_item():
 	var chance_sum: float = 0
 	for item in items:
@@ -32,3 +30,6 @@ func pick_weighted_random_item():
 		if random_chance <= 0:
 			return item
 	return items[0]
+
+func _on_button_pressed() -> void:
+	spin()

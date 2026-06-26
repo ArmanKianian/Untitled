6d9d904654ec3e72abd429bf76d7f7c18f561214
ArmanKianian <kianianarman1@gmail.com>
2026-06-26 21:46:03 +0330

    Random unit selector
    Now spinning wheel draw a card from a trait randomly, there are 3 units for each trait right now.

--- scripts/spinning_wheel.gd
+++ scripts/spinning_wheel.gd
@@ -1,26 +1,60 @@
 extends Node2D
 
 # wheel items with chance of getting
-var items: Array = [
-	{"name": "Item1", "level": 1, "damage": 2, "health": 3, "chance": 10.0},
-	{"name": "Item2", "level": 1, "damage": 1, "health": 1, "chance": 10.0},
-	{"name": "Item3", "level": 1, "damage": 2, "health": 1, "chance": 10.0},
-	{"name": "Item4", "level": 1, "damage": 1, "health": 2, "chance": 10.0},
-	{"name": "Item5", "level": 1, "damage": 3, "health": 1, "chance": 10.0},
-	{"name": "Item6", "level": 1, "damage": 1, "health": 3, "chance": 10.0},
-	{"name": "Item7", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
-	{"name": "Item8", "level": 1, "damage": 4, "health": 1, "chance": 10.0},
-] 
+var Traits: Array = [
+	{"name": "Arcane", "chance": 10.0, "units": [
+	{"name": "Arcane1", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Arcane2", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Arcane3", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+]},
+	{"name": "Inventor", "chance": 10.0, "units": [
+	{"name": "Inventor1", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Inventor2", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Inventor3", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+]},
+	{"name": "Guardian", "chance": 10.0, "units": [
+	{"name": "Guardian1", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Guardian2", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Guardian3", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+]},
+	{"name": "Forest", "chance": 10.0, "units": [
+	{"name": "Forest1", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Forest2", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Forest3", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+]},
+	{"name": "Assasins", "chance": 10.0, "units": [
+	{"name": "Assasins1", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Assasins2", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Assasins3", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+]},
+	{"name": "Undead", "chance": 10.0, "units": [
+	{"name": "Undead1", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Undead2", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Undead3", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+]},
+	{"name": "Shield", "chance": 10.0, "units": [
+	{"name": "Shield1", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Shield2", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Shield3", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+]},
+	{"name": "Inferno", "chance": 10.0, "units": [
+	{"name": "Inferno1", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Inferno2", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+	{"name": "Inferno3", "level": 1, "damage": 3, "health": 2, "chance": 10.0},
+]},
+]
 
 func spin():
-	var chosen = pick_weighted_random_item()
-	for i in range(items.size()):
-		if items[i] == chosen:
+	var chosen = pick_weighted_random_item(Traits)
+	for i in range(Traits.size()):
+		if Traits[i] == chosen:
+			chosen = pick_weighted_random_item(chosen["units"])
 			return await rotate_wheel(i, chosen)
+	chosen = pick_weighted_random_item(chosen["units"])
 	return chosen
 
 # Normalize Chance then pick one random based on weight(chance)
-func pick_weighted_random_item():
+func pick_weighted_random_item(items):
 	randomize()
 	var chance_sum: float = 0
 	for item in items:
